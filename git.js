3c92569cc0360b4d15b75848afe9a6778b5c297a
charlotte.menou <charlotte.menou@depinfoad.univ-brest.fr>
2024-01-17 08:30:45 +0100

    création de l'objet aPerson + ajout d'une fonction sayHello pour que l'objet aPerson puisse se présenter

--- git.js
+++ git.js
@@ -7,3 +7,25 @@ const operand2 = 45;
 let result = operand + operand2;
 
 console.log("resultat de" + operand + "+" + operand2 + "=" + result);
+
+b += a ;
+console.log("resultat de a + b = " + b);
+
+let aPerson = { 
+	name: 'Jean',
+	age: 35,
+	gender: 'masculin',
+	interest: ['musique', 'Jeux videos'],
+};
+
+// on peut également déclarer et initialiser un objet de la façon suivante :
+
+let laPerson = new Object();
+laPerson.name = 'Jean';
+laPerson.age = 35;
+laPerson.gender = 'masculin';
+laPerson.interest = ['musique', 'badminton'];
+
+aPerson.sayHello = function() {console.log('Bonjour ! Je suis ' + this.name + '.'); };
+
+laPerson.sayHello();
